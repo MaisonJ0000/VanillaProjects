807c872f7d87a013f8532dc4438612a96ff6ed64
MaisonJ0000 <maison.J0000@gmail.com>
2021-01-11 01:55:51 +0900

    Until 20!

---
+++ 19_SortableList/script.js
@@ -0,0 +1,115 @@
+const draggableList = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Larry Ellison',
+  'Mark Zuckerberg',
+  'Michael Bloomberg',
+  'Larry Page',
+];
+
+const listItems = [];
+
+const debug = false;
+
+let dragStartIndex;
+
+function dragStart() {
+  if (debug) console.info('Event: ', 'dragStart');
+  dragStartIndex = this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+  if (debug) console.info('Event: ', 'dragEnter');
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  if (debug) console.info('Event: ', 'dragLeave');
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  if (debug) console.info('Event: ', 'dragOver');
+  e.preventDefault();
+}
+
+const swapItems = (fromIndex, toIndex) => {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+};
+
+function dragDrop() {
+  if (debug) console.info('Event: ', 'dragDrop');
+  const dragEndIndex = this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+const addEventListeners = () => {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
+};
+
+const createList = () => {
+  [...richestPeople].map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
+
+      listItem.setAttribute('data-index', index);
+
+      listItem.innerHTML = `
+      <span class="number">${index + 1}</span>
+      <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-lines"></i>
+      </div>
+    `;
+
+      listItems.push(listItem);
+
+      draggableList.appendChild(listItem);
+    });
+
+  addEventListeners();
+};
+
+const checkOrder = () => {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector('.draggable').innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
+};
+
+createList();
+
+check.addEventListener('click', checkOrder);
